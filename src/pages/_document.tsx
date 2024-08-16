45faa98d520e151a616a88611fd1783dd8db68ae
Krinal Sojitra <krinalSojitra21@gmail.com>
2024-08-16 11:36:02 +0530

    chore: Add @vercel/analytics package for page analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
+import {Html, Head, Main, NextScript} from "next/document";
+import {Analytics} from "@vercel/analytics/react";
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
         <Main />
-        <NextScript />
+        <NextScript /> <Analytics />
       </body>
     </Html>
   );
